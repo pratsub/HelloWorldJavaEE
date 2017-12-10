94c7b943013dde6e61a8188f03352f3a22431e8a
SUBHASIS PRATIHARI <subhasis.pratihari@gmail.com>
2017-12-10 16:33:36 +0530

    File updated for controller

---
+++ src/test/java/com/qaagility/javaee/HelloServletControllerTest.java
@@ -0,0 +1,39 @@
+package com.qaagility.javaee;
+
+import com.qaagility.javaee;.HelloServletController;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+public class HelloServletControllerTest{
+   
+  @Before
+    public void setUp() {
+        System.out.println("Before Testing");
+    }
+
+   
+  @Test
+  public void testController(){
+               
+       HelloServletController controller = new HelloServletController(); 
+        assertTrue(controller.returnName().contains("Subhasis"));
+  }
+  /*
+  
+  @Test
+  public void testFailedName(){
+     when(request.getParameter("yourName")).thenReturn("WrongName");
+        when(request.getParameter("password")).thenReturn("secret");
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(writer);
+
+        new MyServlet().doPost(request, response);
+
+        verify(request, atLeast(1)).getParameter("username"); // only if you want to verify username was called...
+        writer.flush(); // it may not have been flushed yet...
+        assertTrue(writer.toString().contains("Subhasis"));
+  } */
+  
+}
